6b3df8a1f315556a3087f621296fa1831ba1ea54
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-03-15 00:16:05 +0700

    some update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
-// const usersRoutes = require('./users');
+const employeeRoutes = require('./employee');
 
 function route(app) {
+    app.use('/employee', employeeRoutes);
 }
 
 module.exports = route
